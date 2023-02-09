9b3c78d6e64d2c34a90d29bc918798c9426bd4d7
Ashanti-Morgan <122491032+Ashanti-Morgan@users.noreply.github.com>
2023-02-08 22:49:08 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -52,19 +52,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Ashanti");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("Land some high quality internships over the next few years");
+        System.out.println("Have a job offer lined up before I graduate");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("Reading");
+        System.out.println("Learning about the tech industry");
+        System.out.println("Video games");
+        System.out.println("Weightlifting");
 
         System.out.println();
         
